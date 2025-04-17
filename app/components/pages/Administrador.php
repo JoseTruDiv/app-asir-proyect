845e0943919bbb65600d5d8dbf0963c70dce8959
@josetrujisilv.disenio <josetrujisilv.disenio@gmail.com>
2025-04-17 16:01:23 +0200

    Gestos usuarios back 17-04-2025

--- app/components/pages/Administrador.php
+++ app/components/pages/Administrador.php
@@ -1,8 +1,41 @@
 <?php
     if ($_SESSION['idRol']==1) {
 ?>
-<div class="container">
-    <h1>Estas en administrador</h1>
+<?php
+    include $GLOBALS['path']->HEADER;
+?>
+
+<div class="container px-3 pt-3 g-1" style="height: 77vh;flex-direction: column;">
+    <div class="d-flex">
+        <h3>Panel de administración de usuarios</h3>
+    </div>
+    <div class="d-flex justify-between">
+        <div>
+            <a href="#" class="btn-auth text d-flex align-center" style="padding: 1rem;text-decoration: none;width: 11rem;gap: 10px;">
+                <svg xmlns="http://www.w3.org/2000/svg" height="20" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/><path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+                </svg> 
+                Añadir usuario</a>
+        </div>
+        <div class="d-flex justify-end align-end g-1 pr-1">
+            <div class="f-flex align-end">
+                <a href="#" id="editUser" class="a-disabled"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
+                </svg></a>
+                <a href="#" id="delUser" class="a-disabled"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
+                </svg></a>
+            </div>
+            <?php 
+                include $GLOBALS['path']->WIDGETS.'/paginations/PaginationsUser.php';
+            ?>
+        </div>
+    </div>
+    <div class="d-flex">
+        <?php 
+            include $GLOBALS['path']->TABLEUSER;
+        ?>
+    </div>
 </div>
 <?php
     }else{
---
+++ app/components/widgets/tables/TableUsers.php
@@ -0,0 +1,31 @@
+<div class="d-flex w-100">
+    <table class="table-user w-100">
+        <tr class="tr-first">
+            <td class="td-check">Check</td>
+            <td>idUser</td>
+            <td>Nombre</td>
+            <td>Apellido</td>
+            <td>Email</td>
+            <td>Departamento</td>
+            <td>Rol</td>
+        </tr>
+        <tr>
+            <td class="td-check"><input type="checkbox" name="" id=""></td>
+            <td>idUser</td>
+            <td>Nombre</td>
+            <td>Apellido</td>
+            <td>Email</td>
+            <td>Departamento</td>
+            <td>Rol</td>
+        </tr>
+        <tr>
+            <td class="td-check"><input type="checkbox" name="" id=""></td>
+            <td>idUser</td>
+            <td>Nombre</td>
+            <td>Apellido</td>
+            <td>Email</td>
+            <td>Departamento</td>
+            <td>Rol</td>
+        </tr>
+    </table>
+</div>
