28df8ed081b83cfb8b4a24d869b017585a46b2f9
@josetrujisilv.disenio <josetrujisilv.disenio@gmail.com>
2025-04-17 18:58:45 +0200

    back 17-04-2025 final

--- app/components/widgets/tables/TableUsers.php
+++ app/components/widgets/tables/TableUsers.php
@@ -1,3 +1,8 @@
+
+
+
+
+
 <div class="d-flex w-100">
     <table class="table-user w-100">
         <tr class="tr-first">
@@ -12,29 +17,65 @@
 
         <?php
 
-            $path = $GLOBALS['path'];
-            require_once $path->CONTROLLERS.'/GetUsersController.php';
+$path = $GLOBALS['path'];
+require_once $path->CONTROLLERS.'/GetUsersController.php';
 
+$getUserController = new GetUsersController();
+
+$arrayUsers = $getUserController->getUsers();
+
+if ($arrayUsers == 0) {
+
+?>
+
+
+<div>
+    <h3>Esta tabla esta vacía</h3>
+</div>
+
+
+<?php
+
+
+
+}else{
+
+    foreach ($arrayUsers as $array) {
+?>
+    <tr>
+    <td class="td-check"><input type="checkbox" name="<?php echo 'idUser'.$array[0];  ?>" id="<?php echo 'idUser'.$array[0];  ?>"></td>
+    <td><?php echo $array[0];  ?></td>
+    <td><?php echo $array[1];  ?></td>
+    <td><?php echo $array[2];  ?></td>
+    <td><?php echo $array[3];  ?></td>
+    <td><?php echo $array[4];  ?></td>
+    <td>
+        <?php
+
+            switch ($array[5]) {
+                case 1:
+                    echo 'Administrador';
+                    break;
+                case 2:
+                    echo 'Usuario';
+                    break;
+                case 3:
+                    echo 'Desarrollador';
+                    break;
+            }
 
         ?>
+    </td> 
+    </tr>
+      
 
-        <tr>
-            <td class="td-check"><input type="checkbox" name="" id=""></td>
-            <td>idUser</td>
-            <td>Nombre</td>
-            <td>Apellido</td>
-            <td>Email</td>
-            <td>Departamento</td>
-            <td>Rol</td>
-        </tr>
-        <tr>
-            <td class="td-check"><input type="checkbox" name="" id=""></td>
-            <td>idUser</td>
-            <td>Nombre</td>
-            <td>Apellido</td>
-            <td>Email</td>
-            <td>Departamento</td>
-            <td>Rol</td>
-        </tr>
+
+<?php
+
+
+    }
+ }
+
+?>
     </table>
 </div>
